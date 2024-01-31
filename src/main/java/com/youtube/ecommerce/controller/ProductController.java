523ecbe23fe301e2bb3c8062fd02020a048f5bd0
marichamy <gmarichamy45@gmail.com>
2024-01-31 22:02:56 +0530

    First commit

--- src/main/java/com/youtube/ecommerce/controller/ProductController.java
+++ src/main/java/com/youtube/ecommerce/controller/ProductController.java
@@ -5,20 +5,20 @@ import com.youtube.ecommerce.entity.Product;
 import com.youtube.ecommerce.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @RestController
 public class ProductController {
     @Autowired
     private ProductService productService;
+    @PreAuthorize("hasRole('Admin')")
     @PostMapping(value={"/addNewProduct"}, consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
     public Product addNewProduct(@RequestPart("product") Product product,
                                  @RequestPart("imageFile")MultipartFile[] file){
@@ -45,6 +45,13 @@ public class ProductController {
             imageModels.add(imageModel);
         }
         return imageModels;
-
+    }
+    @GetMapping({"/getAllProducts"})
+    public List<Product>getAllProducts(){
+       return productService.getAllProducts();
+    }
+    @DeleteMapping({"/deleteProductDetails/{productId}"})
+    public void deleteProductDetails(@PathVariable("productId") Integer productId){
+        productService.deleteProductDetails(productId);
     }
 }
--- src/main/java/com/youtube/ecommerce/service/ProductService.java
+++ src/main/java/com/youtube/ecommerce/service/ProductService.java
@@ -5,6 +5,8 @@ import com.youtube.ecommerce.entity.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class ProductService {
 
@@ -12,7 +14,13 @@ public class ProductService {
     private ProductDao productDao;
     public Product addNewProduct(Product product)
     {
-        productDao.save(product);
         return productDao.save(product);
+    }
+    public List<Product> getAllProducts(){
+        return(List<Product>) productDao.findAll();
+    }
+
+    public void deleteProductDetails(Integer productId){
+        productDao.deleteById(productId);
     }
 }
